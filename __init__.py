c47d9c79a87481e1559df511637fad771fe0e625
scribblemaniac <scribblemaniac@users.noreply.github.com>
2015-12-30 21:06:26 -0700

    Added basic import functionality

--- __init__.py
+++ __init__.py
@@ -4,6 +4,17 @@ import renderchan_importer
 importlib.reload(renderchan_importer)
 from renderchan_importer import RenderChanImporter
 
+bl_info = {
+    "name": "Import rendered files with RenderChan",
+    "author": "scribblemaniac",
+    "version": (0, 1),
+    "blender": (2, 76, 0),
+    "location": "File > Import > RenderChan Dependency",
+    "description": "Imports various file types and renders them with RenderChan so they can be imported into Blender.",
+    "warning": "This requires RenderChan, and may require other binaries depending on the file being rendered",
+    "category": "Import-Export"
+}
+
 def add_import_button(self, context):
     self.layout.operator(RenderChanImporter.bl_idname, text="RenderChan Dependency")
 
--- renderchan_importer.py
+++ renderchan_importer.py
@@ -1,34 +1,23 @@
+import bpy
 from bpy.types import Operator
 from bpy_extras.io_utils import ImportHelper
+import subprocess
+import os.path
 
 class RenderChanImporter(Operator, ImportHelper) :
     bl_idname = "import.renderchan"
     bl_label = "Import with RenderChan"
     
     def execute(self, context) :
-        """
-        filename_ext = ".schematic"
-        
-        filter_glob = StringProperty(default="*.schematic", options={'HIDDEN'},)
-
-        if self.filepath.split('\\')[-1].split('.')[1].lower() != 'schematic':
-            print ("  Selected file = ", self.filepath)
-            raise IOError("The selected input file is not a *.schematic file")
-
-        nbtfile = nbt.nbt.NBTFile(self.filepath,'rb')
-
-        height = nbtfile["Height"].value
-        width = nbtfile["Width"].value
-        length = nbtfile["Length"].value
-
-        bpy.data.scenes[0].render.engine = "CYCLES"
-        bpy.data.scenes[0].render.fps = 20
-        bpy.data.scenes[0].render.fps_base = 1
-        bpy.context.user_preferences.system.use_mipmaps = False
-        bpy.types.Object.blockId = bpy.props.IntProperty(name="Block ID", description="Stores the id of this object's block", default=0)
-        bpy.types.Object.blockMetadata = bpy.props.IntProperty(name="Block Metadata", description="Stores the metadata of this object's block", default=0)
-        for index, dataValue in enumerate(nbtfile["Blocks"].value):
-            if dataValue != 0:
-                self._blockManager.draw(context, index % width - math.floor(width / 2), length - math.floor((index % (width * length)) / width) - math.ceil(length / 2) - 1, math.floor(index / (width * length)), dataValue, nbtfile["Data"][index])
-        """
+        try :
+            subprocess.check_call(["renderchan", self.filepath]);
+            # Can't use this until RenderChan adds this feature
+            # This operation relies on too much code to just copy it over from RenderChan's source
+            #outputFile = subprocess.check_output(["renderchan", "--dry-run", "--no-deps", self.filepath])
+            # For the moment we can use this naive substitute that assumes the structure and format
+            outputFile = os.path.join(os.path.dirname(self.filepath), "render", os.path.basename(self.filepath) + ".png")
+        except subprocess.CalledProcessError as e:
+            self.report({"ERROR","RenderChan encountered an error"})
+            return {"FINISHED"}
+        bpy.ops.image.open(filepath=outputFile)
         return {"FINISHED"}
